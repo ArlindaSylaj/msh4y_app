ea68b07dd148537594f4413362a36e2284297453
Arlinda Sylaj <arlindasylaj14@gmail.com>
2021-03-11 15:06:38 +0100

    constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -28,3 +28,28 @@ const kCoursesHeadings = TextStyle(
   color: Colors.black,
   fontSize: 18.0,
 );
+const kHomePageHeadings = TextStyle(
+  color: Colors.white ,
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+);
+const kHomePageParagraphs = TextStyle(
+  color: Colors.white,
+    fontSize: 13.0
+);
+const kTextFieldDecoration = InputDecoration(
+  hintText: '',
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueGrey, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueGrey, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
