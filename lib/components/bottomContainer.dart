4e6a61163dfdcc6745bf75f7f7990f4d8a878c81
Arlinda Sylaj <arlindasylaj14@gmail.com>
2021-03-11 15:09:02 +0100

    bottom container

--- lib/components/bottomContainer.dart
+++ lib/components/bottomContainer.dart
@@ -11,89 +11,95 @@ Widget build(BuildContext context) {
   return Container(
     color: Color(0xff404040),
     child: Padding(
-      padding: const EdgeInsets.only(left:8.0),
+      padding: const EdgeInsets.only(left:8.0,bottom: 45.0),
       child: Column(
         children: <Widget>[
-          Image.asset('images/logo.png',),
-          Row(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text('What we do\n',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
+          Image.asset('images/logo.png'),
+          Container(
+            constraints: BoxConstraints.expand(
+              height: 200.0, width: 1000,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text('What we do\n',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
                     ),
-                  ),
-                  Text('Tutorials \n'
-                      'Frontend\n'
-                      'Backend\n'
-                      'Mobile',
-                    style: kParagraphStyle,
-                  ),
-                ],
-              ),
+                    Text('Tutorials \n'
+                        'Frontend\n'
+                        'Backend\n'
+                        'Mobile',
+                      style: kParagraphStyle,
+                    ),
+                  ],
+                ),
 //                      SizedBox(
 //                        width: 10.0,
 //                      ),
-              Column(
-                children: <Widget>[
-                  Text('About us\n',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
+                Column(
+                  children: <Widget>[
+                    Text('About us\n',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Text('About us\n'
-                      'A team to help you\n'
-                      'Feedbacks\n'
-                      'Contact',
-                    style: kParagraphStyle,
-                  ),
-                ],
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Column(
-                children: <Widget>[
-                  Text('Legal\n',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
+                    SizedBox(
+                      height: 5.0,
                     ),
-                  ),
-                  Text('Privacy policy\n'
-                      'Website terms\n'
-                      'Cookie policy\n'
-                      'Conflicts policy',
-                    style: kParagraphStyle,
-                  ),
-                ],
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Column(
-                children: <Widget>[
-                  Text('Site Info\n',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
+                    Text('About us\n'
+                        'A team to help you\n'
+                        'Feedbacks\n'
+                        'Contact',
+                      style: kParagraphStyle,
                     ),
-                  ),
-                  Text('Support\n'
-                      'FAQ\n'
-                      'Sitemap\n'
-                      'Careers',
-                    style: kParagraphStyle,
-                  ),
-                ],
-              )
-            ],
+                  ],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text('Legal\n',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                    Text('Privacy policy\n'
+                        'Website terms\n'
+                        'Cookie policy\n'
+                        'Conflicts policy',
+                      style: kParagraphStyle,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text('Site Info\n',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                    Text('Support\n'
+                        'FAQ\n'
+                        'Sitemap\n'
+                        'Careers',
+                      style: kParagraphStyle,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ],
       ),
